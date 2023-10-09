3b799fb8ae7acf3dedeb77e97d62369bdf861371
bogdan <bogdan@udev.dev>
2023-10-09 20:28:54 +0300

    - add validate for input

--- script.js
+++ script.js
@@ -12,10 +12,38 @@ const button = document.getElementById("myButton");
 button.onclick = () => {
     const input = document.getElementById("myInput");
 
-    input.value = 'hello';
 
     // select1.options[select1.options.selectedIndex].text = input.value;
     console.log("hello world!", input.value, select1.options[select1.options.selectedIndex].text)
 }
+
+function validation(){
+
+  function createError(input, text){
+    const parent = input.parentNode;
+    parent.classList.add('error')
+    console.log(parent)
+  }
+
+  let result = true;
+
+  const allInputs = form.querySelectorAll('input')
+
+  for (const input of allInputs){
+
+    if(input.value==""){
+      console.log('error');
+      createError('Поле не заполнено')
+      result = false
+    }
+  }
+  return result
+}
+ document.getElementById('form').addEventListener('add-form', function (event) {
+   event.preventDefault()
+    if (validation(this) == true) {
+      alert('Форма успешно проверена')
+    }
+ })
 
 // log() => console.log("hello world!");
